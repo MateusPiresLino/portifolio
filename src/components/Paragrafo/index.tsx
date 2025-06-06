a47c2d4d9b304dacd5710a5f579db664a0888fb5
MateusPiresLino <mateuspireslino@gmail.com>
2025-06-05 22:04:08 -0300

    estilizando a Sidebar

--- src/components/Paragrafo/index.tsx
+++ src/components/Paragrafo/index.tsx
@@ -3,10 +3,13 @@ import { Par } from './styles'
 export type Props = {
   children: string
   tipo?: 'principal' | 'secundario'
+  fontSize?: number
 }
 
-const Paragrafo = ({ children, tipo = 'principal' }: Props) => (
-  <Par tipo={tipo}>{children}</Par>
+const Paragrafo = ({ children, tipo = 'principal', fontSize }: Props) => (
+  <Par fontSize={fontSize} tipo={tipo}>
+    {children}
+  </Par>
 )
 
 export default Paragrafo
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,10 +1,23 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Title from '../../components/Title'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 // isso é o components para criação do aside no html
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/MateusPiresLino.png" alt="" />
-    <Title fontSize={20}>Mateus Lino</Title>
+    <SidebarContainer>
+      <Avatar></Avatar>
+      <Title fontSize={20}>Mateus Lino</Title>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        Omateuslino
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro Front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
